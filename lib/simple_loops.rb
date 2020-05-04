d070f878672a045ac44a5ec2be36a274e305b1ed
Justin James <justin.james@yale.edu>
2020-05-04 15:53:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,7 +16,7 @@ def loop_message_n_times(string, integer)
   end
 end
 
-def output_array(message, integer)
+def output_array(array)
   counter = 0 
   while array[counter] < array.length do 
     puts array[counter]
